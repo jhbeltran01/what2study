ca4dc99786b89039b9a94c3800fe9fe5bbf453d5
ligaya-milangela <jodacuba@gbox.adnu.edu.ph>
2024-09-07 00:48:16 +0800

    configurations in to env file

--- backend/apps/pages/views.py
+++ backend/apps/pages/views.py
@@ -5,9 +5,14 @@ from django.contrib.auth import logout
 
 
 def home(request):
-    print("safsadf")
     return render(request, "pages/home.html")
 
 def logout_view(request):
     logout(request)
     return redirect("/")
+
+class dashboard(TemplateView):
+    print("efhlwknschi")
+    template_name = 'dashboard'
+    def get(self, request):
+        return render(request, 'pages/dashboard.html')
