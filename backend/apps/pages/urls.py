2cca71c5c3b1a2b4dbfc43c6bae4ce4e7648bd05
ligaya-milangela <jodacuba@gbox.adnu.edu.ph>
2024-09-11 17:12:16 +0800

    reviewer api

--- backend/apps/pages/urls.py
+++ backend/apps/pages/urls.py
@@ -8,7 +8,7 @@ app_name = 'pages'
 urlpatterns = [
     path('', views.LandingPageView.as_view(), name='landing-page'),
     path("logout", views.logout_view),
-    path('dashboard/', views.dashboard.as_view(), name='dashboard'),
+    path('dashboard/', views.DashboardPageView.as_view(), name='dashboard'),
     path('reviewer/', check_user_exists),
 
 
