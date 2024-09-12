85ec787d1dda0315fd81612809212ae2a89f4cc4
jhbeltran01 <jhbeltran2001@gmail.com>
2024-09-12 11:37:23 +0800

    fix issues

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,12 +10,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { rootPath } from './globals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='app'>
+    <BrowserRouter basename={rootPath}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
