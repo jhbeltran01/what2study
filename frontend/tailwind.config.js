95e3268a965917bf7e9017c7774aee9dda0f9fc2
jhbeltran01 <jhbeltran2001@gmail.com>
2024-10-20 12:44:21 +0800

    fix home page UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  content: ['./dist/*.html', './src/**/*.vue', './templates/**/*.html'],
+  content: [
+    './dist/*.html', 
+    './src/**/*.vue', 
+    './templates/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx'
+  ],
   theme: {
     extend: {},
   },
