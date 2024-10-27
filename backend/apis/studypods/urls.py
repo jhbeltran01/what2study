5c0ae1aa0f3dd7098f036db169ac5498ef654ba6
jhbeltran01 <jhbeltran2001@gmail.com>
2024-10-27 11:10:03 +0800

    implement CRUD on studypod reviewer

--- backend/apis/studypods/urls.py
+++ backend/apis/studypods/urls.py
@@ -14,7 +14,20 @@ urlpatterns = [
     path('join/',
             views.JoinStudypodAPIView.as_view(),
                 name='join'),
+    # for creating a reviewer and fetching the list of reviewers
+    # on the studypod
+    path('<slug:studypod_slug>/reviewers/',
+            views.StudypodReviewersAPIView.as_view(),
+                name='reviewer'),
+    # for updating, deleting and
+    # fetching a single studypod reviewer
+    path('reviewers/<slug:reviewer_slug>/',
+            views.StudypodReviewersAPIView.as_view(),
+                name='single-reviewer'),
     path('<slug:slug>/',
             views.StudyPodAPIView.as_view(),
                 name='slug'),
+    path('<slug:slug>/leave/',
+            views.LeaveStudypodAPIView.as_view(),
+                name='leave'),
 ]
